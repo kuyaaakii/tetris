b2765f6658995f07b102f174fd5ad1def9442930
kuyaaakii <borlongan.family0811@gmail.com>
2021-07-21 13:40:19 +0800

    made changes to main.py

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import pygame
-blocksize = 54
 pygame.init()
-infoObject = pygame.display.Info()
-WIDTH, HEIGHT = infoObject.current_w, infoObject.current_h
-win = pygame.display.set_mode((1920,1080)) 
+WIDTH, HEIGHT = pygame.display.Info().current_w, pygame.display.Info().current_h
+blocksize = HEIGHT//20
+win = pygame.display.set_mode((0,0),pygame.FULLSCREEN) 
 pygame.display.set_caption("tetris")
 
-def drawplayarea():
+def drawplayareaborder():
+
+    diff = (WIDTH//3 - HEIGHT//2)//2
+    print(diff)
     blok = 0
-    x = WIDTH//3+50
-    xdiff = WIDTH//3*2-50
+    x = WIDTH//3+diff
+    xdiff = WIDTH//3*2-diff
+
     for i in range(20):
-        print (xdiff)
-        print(x)
         rect = pygame.Rect(x, blok, blocksize, blocksize)
         rectdiff = pygame.Rect(xdiff, blok, blocksize, blocksize)
+
         pygame.draw.rect(win, (0, 0, 255), rect)
         pygame.draw.rect(win, (0, 0, 255), rectdiff)
         pygame.draw.rect(win, (0, 0, 0), rect,1)
         pygame.draw.rect(win, (0, 0, 0), rectdiff,1)
-        blok = blok + 54
+
+        blok = blok + blocksize
     
 
 
@@ -29,7 +32,7 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        drawplayarea()
+        drawplayareaborder()
         pygame.display.update()
     pygame.quit()        
 
